e7cfa1b3d1465e5c4a37477b015d984e4afb0924
Slegends <negisachin750@gmail.com>
2024-05-24 23:46:44 +0530

    Fixed eslintrc file

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,4 +1,4 @@
-Mern-SocialMedia/frontend/.prettierignoremodule.exports = {
+module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-  },
-  server: {
-    port: 3000,
-    open: true,
-    cors: true,
-  },
-  base: "./",
 })
