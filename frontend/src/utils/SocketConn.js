9587ca97a6bc28667f7cffb409e07d78dcbe5d6d
Slegends <negisachin750@gmail.com>
2025-08-06 11:42:43 +0530

    Fixed Sockets

--- frontend/src/utils/SocketConn.js
+++ frontend/src/utils/SocketConn.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client"
 
-// const socket = io.connect("https://letztalk.onrender.com");
-const socket = io.connect("http://localhost:5174");
+const socket = io.connect("https://letztalk.onrender.com");
+// const socket = io.connect("http://localhost:5174");
 console.log("Socket init");
 
 if(!socket){
